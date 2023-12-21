abffbfb5a6f848175e41610a75e56fb5c338d7ab
amrutha <amruthamurali944@gmail.com>
2023-12-21 16:03:52 +0530

    dumped

--- shoppingcart/tasks.py
+++ shoppingcart/tasks.py
@@ -1,11 +1,11 @@
-from celery import shared_task
+# from celery import shared_task
 from django.core.mail import send_mail
 from django.template.loader import render_to_string
 from django.utils.html import strip_tags
 from django.conf import settings
 from .models import *
 
-@shared_task
+# @shared_task
 def send_order_confirmation_email(order_id, user_email):
     order = Order.objects.get(id=order_id)
     subject = 'Order Confirmation'
